1c58ea3e3850f131d8f683352773bb92fa457fca
yimingy72 <50072883+yimingy72@users.noreply.github.com>
2024-08-15 21:49:24 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.sstty.top": dockerHub,
+  "quay.sstty.top": "https://quay.io",
+  "gcr.sstty.top": "https://gcr.io",
+  "k8s-gcr.sstty.top": "https://k8s.gcr.io",
+  "k8s.sstty.top": "https://registry.k8s.io",
+  "ghcr.sstty.top": "https://ghcr.io",
+  "cloudsmith.sstty.top": "https://docker.cloudsmith.io",
+  "ecr.sstty.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.sstty.top": dockerHub,
 };
 
 function routeByHosts(host) {
